9241db7282cca60c2bb5911a8d853469a12c824d
Лев Мухачев <163592011+LionMux@users.noreply.github.com>
2025-04-23 15:34:50 +0300

    analize flt

---
+++ CometradeParser.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using Prism.Commands;
+using OxyPlot;
+using OxyPlot.Series;
+using System.Collections.ObjectModel;
+using System;
+
+namespace OscilAnalyzer
+{
+    internal class CometradeParser
+    {
+        private List<double> _currentA;
+        private List<double> _currentB;
+        private List<double> _currentC;
+        private List<double> _voltageA;
+        private List<double> _voltageB;
+        private List<double> _voltageC;
+        private List<double> _timeValue;
+        private double numOfPoints;
+
+        private string _cfgFileName = "Files\\25_newRTDS.cfg";
+        private string _datFileName = "Files\\25_newRTDS.dat";
+
+        public List<double> CurrentA { get => _currentA; set => _currentA = value; }
+        public List<double> CurrentB { get => _currentB; set => _currentB = value; }
+        public List<double> CurrentC { get => _currentC; set => _currentC = value; }
+        public List<double> VoltageA { get => _voltageA; set => _voltageA = value; }
+        public List<double> VoltageB { get => _voltageB; set => _voltageB = value; }
+        public List<double> VoltageC { get => _voltageC; set => _voltageC = value; }
+
+        public CometradeParser()
+        {
+
+        }
+
+        public void ReadSignal()
+        {
+
+        }
+    }
+}
--- MathMetod/TypeOfFaultAnalizer.cs
+++ MathMetod/TypeOfFaultAnalizer.cs
@@ -11,18 +11,6 @@ namespace COMTRADE_parser.MathMetod
 {
     internal class TypeOfFaultAnalizer
     {
-        private readonly IDataReader _dataReader;
-        private double _magnitudeI0I2;
-        private double _magnitudeI2I1;
-        private double _angleI2I0;
-        private FourieAnalizer fourieAnalizer_I;
-        private FourieAnalizer fourieAnalizer_V;
-        private double[] current_A;
-        private double[] current_B;
-        private double[] current_C;
-        private double[] voltage_A;
-        private double[] voltage_B;
-        private double[] voltage_C;
         private bool _k3;
         private bool _kab2;
         private bool _kbc2;
@@ -34,7 +22,20 @@ namespace COMTRADE_parser.MathMetod
         private bool _kbc11;
         private bool _kca11;
 
-        public TypeOfFaultAnalizer(IDataReader dataReader)
+        private FourieAnalizer fourieAnalizer_I;
+        private FourieAnalizer fourieAnalizer_U;
+
+        private Complex[] current_A;
+        private Complex[] current_B;
+        private Complex[] current_C;
+        private Complex[] voltage_A;
+        private Complex[] voltage_B;
+        private Complex[] voltage_C;
+        private Complex[] z_A;
+        private Complex[] z_B;
+        private Complex[] z_C;
+
+        public TypeOfFaultAnalizer(FourieAnalizer fourieAnalizer_I, FourieAnalizer fourieAnalizer_U)
         {
             _k3 = false;
             _kab2 = false;
@@ -46,11 +47,103 @@ namespace COMTRADE_parser.MathMetod
             _kab11 = false;
             _kbc11 = false;
             _kca11 = false;
-            _dataReader = dataReader;
-            Currents currents = new Currents(_dataReader);
-            Voltage voltages = new Voltage(_dataReader);
-            fourieAnalizer_I = new FourieAnalizer(_dataReader.AnalogData.Count, _dataReader.Config.Rate, current_A, current_B, current_C);
-            fourieAnalizer_V = new FourieAnalizer(_dataReader.AnalogData.Count, _dataReader.Config.Rate, voltage_A, voltage_B, voltage_C);
+
+            this.fourieAnalizer_I = fourieAnalizer_I;
+            this.fourieAnalizer_U = fourieAnalizer_U;
+
+            current_A = fourieAnalizer_I.fourie_A;
+            current_B = fourieAnalizer_I.fourie_B;
+            current_C = fourieAnalizer_I.fourie_C;
+            voltage_A = fourieAnalizer_U.fourie_A;
+            voltage_B = fourieAnalizer_U.fourie_B;
+            voltage_C = fourieAnalizer_U.fourie_C;
+
+            CalculateImpedance();
+        }
+
+        public void StartFaultAnalize()
+        {
+            for (int i = 0; i < fourieAnalizer_I.Pramaya.Length; i++)
+            {
+                if ((fourieAnalizer_I.Nulevaya[i].Magnitude / fourieAnalizer_I.Obratnaya[i].Magnitude) < 0.1)
+                {
+                    TESTFLT2P(i);
+                    TESTFLT3P(i);
+                }
+                if ( (fourieAnalizer_U.Pramaya[i] / fourieAnalizer_I.Pramaya[i]).Phase > (-60 * Math.PI / 180) &&
+                    (fourieAnalizer_U.Pramaya[i] / fourieAnalizer_I.Pramaya[i]).Phase < (60 * Math.PI / 180))
+                {
+                    if (2 * z_A[i].Magnitude > z_B[i].Magnitude && 2 * z_A[i].Magnitude > z_C[i].Magnitude)
+                    {
+                        _ka1 = true;
+                    }
+                    if (0.5 * z_A[i].Magnitude > z_B[i].Magnitude && 0.5 * z_A[i].Magnitude > z_C[i].Magnitude)
+                    {
+                        _kbc11 = true;
+                    }
+                }
+                else if ((fourieAnalizer_U.Pramaya[i] / fourieAnalizer_I.Pramaya[i]).Phase > (60 * Math.PI / 180) &&
+                    (fourieAnalizer_U.Pramaya[i] / fourieAnalizer_I.Pramaya[i]).Phase < (180 * Math.PI / 180))
+                {
+                    if (2 * z_C[i].Magnitude > z_A[i].Magnitude && 2 * z_C[i].Magnitude > z_B[i].Magnitude)
+                    {
+                        _kc1 = true;
+                    }
+                    if (0.5 * z_C[i].Magnitude > z_A[i].Magnitude && 0.5 * z_C[i].Magnitude > z_B[i].Magnitude)
+                    {
+                        _kab11 = true;
+                    }
+                }
+                else if ((fourieAnalizer_U.Pramaya[i] / fourieAnalizer_I.Pramaya[i]).Phase > (-180 * Math.PI / 180) &&
+                    (fourieAnalizer_U.Pramaya[i] / fourieAnalizer_I.Pramaya[i]).Phase < (-60 * Math.PI / 180))
+                {
+                    if (2 * z_B[i].Magnitude > z_A[i].Magnitude && 2 * z_B[i].Magnitude > z_C[i].Magnitude)
+                    {
+                        _kb1 = true;
+                    }
+                    if (0.5 * z_B[i].Magnitude > z_A[i].Magnitude && 0.5 * z_B[i].Magnitude > z_C[i].Magnitude)
+                    {
+                        _kca11 = true;
+                    }
+                }
+            }
+        }
+
+        public void CalculateImpedance()
+        {
+            for (int i = 0; i < current_A.Length; i++)
+            {
+                z_A[i] = current_A[i] / voltage_A[i];
+                z_B[i] = current_B[i] / voltage_B[i];
+                z_C[i] = current_C[i] / voltage_C[i];
+            }
+        }
+
+        public void TESTFLT2P(int i)
+        {
+
+            if (0.5 * z_C[i].Magnitude > z_A[i].Magnitude && 0.5 * z_C[i].Magnitude > z_B[i].Magnitude)
+            {
+                _kab2 = true;
+            }
+            else if (0.5 * z_A[i].Magnitude > z_B[i].Magnitude && 0.5 * z_A[i].Magnitude > z_C[i].Magnitude)
+            {
+                _kbc2 = true;
+            }
+            else if (0.5 * z_B[i].Magnitude > z_A[i].Magnitude && 0.5 * z_B[i].Magnitude > z_C[i].Magnitude)
+            {
+                _kca2 = true;
+            }
+        }
+        public void TESTFLT3P(int i)
+        {
+
+            if ( (fourieAnalizer_I.Nulevaya[i].Magnitude / fourieAnalizer_I.Obratnaya[i].Magnitude) < 0.1 &&
+                Math.Abs( (fourieAnalizer_U.Pramaya[i] / fourieAnalizer_I.Pramaya[i] ).Phase) > ( 30 * Math.PI / 180) )
+            {
+                _k3 = true;
+            }
+
         }
     }
 }
